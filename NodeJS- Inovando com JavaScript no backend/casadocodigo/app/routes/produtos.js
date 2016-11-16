9821e3975790ad1f7608ea41b8bcaab2f7301b02
IcaroCleto <icarocleto.sousa@outlook.com>
2016-11-16 17:45:52 -0200

    Class 3 complete

--- NodeJS- Inovando com JavaScript no backend/casadocodigo/app/routes/produtos.js
+++ NodeJS- Inovando com JavaScript no backend/casadocodigo/app/routes/produtos.js
@@ -1,19 +1,35 @@
 module.exports = function(app){
+    var listaProdutos = function(request, response){
+        //Importa a função conexão do arquivo connectionFactory.js
+        var connection = app.infra.connectionFactory();  
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.lista(function(err,result){
+            response.render('produtos/lista',{lista:result});
+        });
+        connection.end(); 
+    }
     app.get('/',function(request, response){
     response.send("<html><body>Página Inicial</body></html>"); // Renderiza a mensagem.
     // response.render("produtos/lista"); // Renderiza a página. OBS: A pasta view é padrão do express.
 });
 
-    app.get("/produtos", function(request,response){
-        //Importa a função conexão do arquivo connectionFactory.js
+    app.get("/produtos", listaProdutos);
+
+    app.get("/produtos/form", function(request, response){
+        response.render('produtos/form');
+    });
+
+    app.post("/produtos", function(request, response){
+
+        var produto = request.body;
+
         var connection = app.infra.connectionFactory();  
-        var produtosBanco = new app.infra.ProdutosDAO(connection);
-
-        produtosBanco.lista(function(err,result){
-            response.render('produtos/lista',{lista:result});
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.salva(produto, function(error, result){
+            produtosDAO.lista(function(err,result){
+                response.redirect('produtos/lista',{lista:result});
+            });
         });
-
-        connection.end();   
     });
     
     // app.get('produtos/remove', function(){
--- NodeJS- Inovando com JavaScript no backend/casadocodigo/config/express.js
+++ NodeJS- Inovando com JavaScript no backend/casadocodigo/config/express.js
@@ -1,5 +1,6 @@
 var express = require('express'); // Carrega o modulo do express
 var load = require('express-load') //  
+var bodyParser = require('body-parser');
 
 module.exports = function (){   
 
@@ -8,11 +9,13 @@ module.exports = function (){
     app.set('view engine','ejs'); // Configura o engine de layout
     app.set('views','./app/views'); //Configura onde o express busca as views. OBS: A posição é baseada da onde o servidor sobe ou seja App.js.
 
+    app.use(bodyParser.urlencoded({extend: true}));
+
     load('routes', {cwd: 'app'}) // Carrega todos os arquivos dentro de rotas.
         .then('infra') // Então carrega todos os arquivos dentro de infra.
         .into(app); // Tudo que for carregado automaticante fica dentro da app (express).
 
-    return app; // Retorna o app configurado.
+        return app; // Retorna o app configurado.
 }
 
 // Outra maneira
