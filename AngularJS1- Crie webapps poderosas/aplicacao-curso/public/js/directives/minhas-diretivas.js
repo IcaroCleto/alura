d89b11111c767956454928dcf6d7fc0af64a8a6e
IcaroCleto <icarocleto.sousa@outlook.com>
2016-12-04 18:35:20 -0200

    '04/12/2016'

--- AngularJS1- Crie webapps poderosas/aplicacao-curso/public/js/directives/minhas-diretivas.js
+++ AngularJS1- Crie webapps poderosas/aplicacao-curso/public/js/directives/minhas-diretivas.js
@@ -8,7 +8,7 @@ angular.module('minhasDiretivas', [])
 
 
 		ddo.scope = {
-            titulo: '@'
+            titulo: '@' // O @ Só copia o q tiver no value da diretiva com esse nome
         };
 
         ddo.templateUrl = 'js/directives/meu-painel.html';
@@ -22,11 +22,25 @@ angular.module('minhasDiretivas', [])
         ddo.restrict = "AE";
 
         ddo.scope = {
-            titulo: '@',
+            titulo: '@', // O @ Só copia o q tiver no value da diretiva com esse nome
             url: '@'
         };
 
         ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';           
         
+        return ddo;
+    })
+    .directive('meuBotaoPerigo', function(){
+        var ddo = {}
+
+        ddo.restrict = "E";
+
+        ddo.scope = {
+            nome: '@', // O @ só copia o q tiver no value da diretiva com esse nome em forma de string.
+            acao: '&' // Passa uma expressão como por exemplo uma função.
+        };
+
+        ddo.template ='<button ng-click="acao(foto)" class="btn btn-danger btn-block">{{nome}}</button>';
+
         return ddo;
     });
