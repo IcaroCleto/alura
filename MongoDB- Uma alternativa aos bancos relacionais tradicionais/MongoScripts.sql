56327cff22944eb3c718f6d1f51228925179fe8c
Icaro Sousa <icarocleto.sousa@outlook.com>
2016-12-26 12:07:59 -0200

    Curso de Node finalizado

---
+++ MongoDB- Uma alternativa aos bancos relacionais tradicionais/MongoScripts.sql
@@ -0,0 +1,47 @@
+-- Primeiro Aluno
+
+{
+    "nome" : "Alberto",
+    "data_nascimento" : new Date(1972, 08, 30),
+    "curso" : {
+        "nome" : "Medicina"
+    },
+    "habilidades" : [
+        {
+            "nome" : "inglês",
+            "nivel" : "avançado"
+        }
+    ]
+}
+
+-- Segundo Aluno
+
+{
+    "nome" : "Alberto",
+    "data_nascimento" : new Date(1972, 09, 30),
+    "curso" : {
+        "nome" : "Engenharia Química"
+    },
+    "habilidades" : [
+        {
+            "nome" : "inglês",
+            "nivel" : "intermediario"
+        }
+    ]
+}
+
+-- Terceiro Aluno
+
+{
+    "nome" : "Daniela",
+    "data_nascimento" : new Date(1972, 09, 30),
+    "curso" : {
+        "nome" : "Moda"
+    },
+    "habilidades" : [
+        {
+            "nome" : "alemão",
+            "nivel" : "básico"
+        }
+    ]
+}
