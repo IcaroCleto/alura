7a0f0edbbf797617b9d4e2aae4d234800f0f6d67
Icaro Sousa <icarocleto.sousa@outlook.com>
2017-01-02 23:23:51 -0200

    Capitulo 7 finalizado

--- MEAN Stack- Criando aplicações completas com Javascript/alurapic/config/express.js
+++ MEAN Stack- Criando aplicações completas com Javascript/alurapic/config/express.js
@@ -1,7 +1,16 @@
 //Module de configuração do Express
 var express = require('express');
 var app = express();
+var consign = require('consign');
+var bodyParser = require('body-parser');
 
 app.use(express.static('./public'));
+
+app.use(bodyParser.json());
+
+consign({ cwd: 'app' })
+    .include('api')
+    .then('routes')
+    .into(app);
 
 module.exports = app;
